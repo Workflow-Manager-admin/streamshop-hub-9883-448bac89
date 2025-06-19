05a36e54bebb7bbbaa89476abb25c81565a0bcee
Kavia AI <production@kavia.ai>
2025-06-19 11:42:33 +0000

    CheckPoint

--- streamshop_hub_backend/config/settings.py
+++ streamshop_hub_backend/config/settings.py
@@ -131,3 +131,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+      "http://localhost:3000",
+      "https://vscode-internal-40-beta.beta01.cloud.kavia.ai",  # as appropriate
+  ]
